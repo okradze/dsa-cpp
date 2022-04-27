ccb879d08a0626f2d2741ba69b0e5bead8b73920
Mirian Okradze <okradzemirian@gmail.com>
2022-04-27 12:31:30 +0400

    Arrays: Pair of Sum

---
+++ Arrays/pairs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+vector<int> pairSum(vector<int> arr, int target) {
+  unordered_set<int> set;
+  vector<int> result;
+
+  for(int i = 0; i < arr.size(); i++) {
+    int x = target - arr[i];
+
+    if(set.find(x) != set.end()) {
+      result.push_back(x);
+      result.push_back(arr[i]);
+      return result;
+    }
+
+    set.insert(arr[i]);
+  }
+
+  return result;
+}
+
+int main() {
+  vector<int> arr {10, 5, 2, 3, -6, 9, 11};
+  
+  auto p = pairSum(arr, 4);
+
+  if(p.size() == 0) {
+    cout << "No pairs found" << endl;
+  } else {
+    cout << p[0] << " " << p[1] << endl;
+  }
+
+  return 0;
+}
