c00e30e607ad43a46d8219b82eca0901e5f462a9
Mirian Okradze <okradzemirian@gmail.com>
2022-05-05 10:04:27 +0400

    Two Pointers: Is Palindrome

--- Arrays/trapping_rain.cpp
+++ Arrays/trapping_rain.cpp
@@ -4,9 +4,23 @@ using namespace std;
 
 int trap(vector<int> &height)
 {
+    int total = 0;
+    int maxLeft = 0;
+    int maxRight = 0;
+
+    for (int h : height)
+    {
+    }
+
+    return total;
 }
 
 int main()
 {
+    vector<int> height{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
+    cout << trap(height) << endl;
+    cout << height.size() << endl;
+
     return 0;
 }
