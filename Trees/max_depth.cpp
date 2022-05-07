268c6fd1421f5917af61a01a1484b3f60032efcb
Mirian Okradze <okradzemirian@gmail.com>
2022-05-07 22:14:16 +0400

    Tree: Maximum Depth

---
+++ Trees/max_depth.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode *root, int depth = 0)
+{
+  if (root == nullptr)
+    return depth;
+
+  int left = maxDepth(root->left, depth + 1);
+  int right = maxDepth(root->right, depth + 1);
+
+  return max(left, right);
+}
+
+int main()
+{
+  return 0;
+}
